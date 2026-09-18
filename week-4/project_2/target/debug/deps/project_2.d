d1eeb9e677c8ef78e2acf0516749ce7c39dfe395
oosahonomorodion <osakpolo.osahon-omorodion@pau.edu.ng>
2026-09-18 23:26:30 +0100

    Delete week-4/project_2 directory

--- week-4/project_2/target/debug/deps/project_2.d
+++
@@ -1,5 +0,0 @@
-C:\Users\USER\Documents\o.osahon-omorodionCOS101\week-4\project_2\target\debug\deps\project_2.d: src\main.rs
-
-C:\Users\USER\Documents\o.osahon-omorodionCOS101\week-4\project_2\target\debug\deps\project_2.exe: src\main.rs
-
-src\main.rs:
